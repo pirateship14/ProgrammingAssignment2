d8af3b588318a8d7e6610fbe8cd9ee8e0d50b25b
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2015-06-11 22:24:45 +0530

    Refactored code for matrices
    The cache now contains inverses of matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function (makeCacheMatrix) takes as input a matrix and
+## creates a cache to store its inverse, the second function then
+## takes the object returned by the first function and 
+## checks cache for inverse before doing a new computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates getters and setters to 
+## add a matrix and its inverse, if computed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  ##Sets the object's cache to null
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+##cacheSolve first checks the cache of the object for a computed value
+##If none is found, it proceeds with the computation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m) ##Set the cache to contain the inverse
+  m
 }
